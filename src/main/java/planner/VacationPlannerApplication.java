6dced60570c347b9ae5855ec84032f25e0cd62d8
Michael Van Riessen <putout.mikeyj@gmail.com>
2023-04-03 21:23:40 -0500

    Initial project commit

---
+++ src/main/java/planner/VacationPlannerApplication.java
@@ -0,0 +1,13 @@
+package planner;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VacationPlannerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VacationPlannerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/planner/VacationPlannerApplicationTests.java
@@ -0,0 +1,13 @@
+package planner;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VacationPlannerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
