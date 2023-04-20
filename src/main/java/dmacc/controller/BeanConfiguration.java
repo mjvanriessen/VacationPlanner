3a44c4438ef44ed4e7485a266367f126cce4efc3
Julie Burger <julieburger81@gmail.com>
2023-04-19 20:54:04 -0500

    remove passcode

--- src/main/java/dmacc/controller/BeanConfiguration.java
+++ src/main/java/dmacc/controller/BeanConfiguration.java
@@ -48,7 +48,7 @@ public class BeanConfiguration {
 				 .driverClassName("com.mysql.cj.jdbc.Driver")
 		          .url("jdbc:mysql://localhost:3306/vacationplanner?useSSL=false")
 		          .username("root")
-		          .password("NEW$tudent0821")
+		          .password("")
 		          .build();	
 	}
 }
